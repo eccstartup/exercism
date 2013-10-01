76bdc53c6235f7c4131e3671a75d1b00c9f89598
eccstartup <zhiwudazhanjiangshi@gmail.com>
2013-10-01 17:42:29 +0800

    use foldl\' instead as a test

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -8,9 +8,12 @@ import Data.Map (Map, fromList, insertWith)
 import Data.List (foldl')
 
 count :: Char -> String -> Int
-count c
-  | (not $ elem c "ACTGU") = fail ("invalid nucleotide " ++ show c)
-  | otherwise = length . (filter (== c))
+count c s
+  | (not $ elem c "ACTGU") = error ("invalid nucleotide " ++ show c)
+  | otherwise = foldl' (+) 0 $ zipWith equ (repeat c) s
+                where equ a b
+                        | a == b = 1
+                        | otherwise = 0
 
 nucleotideCounts :: String -> Map Char Int
 nucleotideCounts = foldl' (\map key -> insertWith (+) key 1 map) none 
