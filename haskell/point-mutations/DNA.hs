2a988d523e074b1429a9a310cafea872fc16bc9d
eccstartup <zhiwudazhanjiangshi@gmail.com>
2013-10-03 08:16:50 +0800

    use fromEnum in DNA.hs

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -7,5 +7,4 @@ import Data.List (foldl')
 
 hammingDistance :: String -> String -> Int
 hammingDistance n1 n2 = foldl' (+) 0 $ zipWith diff n1 n2
-                        where diff a b = case (compare a b) of EQ -> 0
-                                                               otherwise -> 1
+                        where diff a b = fromEnum $ a /= b
